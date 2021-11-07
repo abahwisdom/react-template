1349c177b2d1d8c04ddf6b9fadf6b66fc41a99fb
Abah <abahwisdom@gmail.com>
2021-11-07 08:51:18 +0100

    unfinished template start

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import circles from './3964.jpg'
+
+
 
 function App() {
+
+
+
   return (
     <div className='contain'>
-    <div className='pad-contain'>
-      {/* <img className='pad' src={circles}/> */}
-     
-    </div>
-    <div className='title-contain'>
-    <div className='pes-title'>
-        <div className='league-name'>E-QUAL17Y LEAGUE</div>
-        <div className='soon'>coming soon</div>
-    </div>
-    </div>
+      
     </div>
   );
 }
 
 export default App;
 
-// Fixtures
-// Results
-// Table
-// News
